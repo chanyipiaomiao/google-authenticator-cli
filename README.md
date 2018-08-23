ad036c4affe564203a1198babd8293136e5011c2
helei <chanyipiaomiao@163.com>
2018-08-23 11:31:11 +0800

    更改key存储方式

--- README.md
+++ README.md
@@ -14,22 +14,22 @@ Google Authenticator 命令行客户端
 ```sh
 google-authenticator-cli add --name="aliyun" --secret=xxxxxxxxxxx
 
---name   指定的是标识，就是为了方便辨认
+--name   指定的是标识，就是为了方便辨认  有空格需要用双引号
 --secret 一般跟两步验证二维码一起展示，方便手动添加的
 ```
 
 识别二维码添加
 
 ```sh
-./google-authenticator-cli add --name="Test" --qrcode=二维码图片路径
+./google-authenticator-cli add --name="Test Test" --qrcode=二维码图片路径
 ```
 
 #### 删除
 
 ```sh
-google-authenticator-cli delete --delete-secret=xxxxxxxxxxx
+google-authenticator-cli delete --delete-name=xxx
 
---delete-secret 和添加功能里面一样
+--delete-name 和添加功能name里面一样
 ```
 
 #### 展示6位数字
@@ -47,11 +47,35 @@ Common 603175 18
 第三列 剩余的时间(秒) 30秒循环一次
 ```
 
-展示指定的secret
+展示指定的Name
+
+Name 有空格需要用双引号引起来
 
 ```sh
-google-authenticator-cli show --show-secret="xxxx"
+google-authenticator-cli show --show-name="xxxx"
 ```
+
+#### 保存6位数字到文件
+
+openvpn客户端连接时，有些配置是需要输入Google Authenticator生成的6位数字, 
+客户端配置文件中有配置: auth-user-pass pass.txt 可以指定一个文件里面存储用户名和密码，
+此功能就是为生成这个文件，方便使用
+
+```sh
+google-authenticator-cli save --save-name="VPN" --username=用户名 --path=./pass.txt
+
+--save-name 指定的上面步骤添加的name
+--username  指定的openvpn客户端连接时需要输入的用户名
+--path      指定文件的路径
+```
+
+生成的文件格式如下: 
+```sh
+用户名
+6位数字
+```
+
+可以定时调用该命令生成文件
 
 #### 可以使用watch命令动态显示
 
--- main.go
+++ main.go
@@ -16,15 +16,7 @@ const (
 	secretTableName = "secret"
 )
 
-func SortMapByKey(m map[string][]byte) []string {
-	var keys []string
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
 
-	return keys
-}
 
 type Secret struct {
 	TwoStepDB *hltool.BoltDB
@@ -39,73 +31,6 @@ func NewSecret() (*Secret, error) {
 	return &Secret{TwoStepDB: twostepDB}, nil
 }
 
-func (s *Secret) Add(t *hltool.TOTP) error {
-	o, err := hltool.StructToBytes(t)
-	if err != nil {
-		return err
-	}
-	err = s.TwoStepDB.Set(map[string][]byte{
-		t.SecretKey: o,
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *Secret) Delete(secret string) error {
-	err := s.TwoStepDB.Delete([]string{secret})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *Secret) List(name string) error {
-	fmt.Printf("%-20s %-15s %-5s\n", "Name", "Number", "Remaining time")
-	fmt.Printf("%-20s %-15s %-5s\n", "----", "------", "--------------")
-
-	if name != "all" {
-		r, err := s.TwoStepDB.Get([]string{name})
-		if err != nil {
-			return err
-		}
-
-		for _, v := range SortMapByKey(r) {
-			var totp *hltool.TOTP
-			err := hltool.BytesToStruct(r[v], totp)
-			if err != nil {
-				continue
-			}
-			n, t, err := hltool.TwoStepAuthGenNumber(totp)
-			if err != nil {
-				continue
-			}
-			fmt.Printf("%-20s %-15s %-5d\n", r[v], n, t)
-		}
-	} else {
-		r, err := s.TwoStepDB.GetAll()
-		if err != nil {
-			return err
-		}
-
-		for _, v := range SortMapByKey(r) {
-			totp := new(hltool.TOTP)
-			err := hltool.BytesToStruct(r[v], totp)
-			if err != nil {
-				continue
-			}
-			n, t, err := hltool.TwoStepAuthGenNumber(totp)
-			if err != nil {
-				continue
-			}
-			fmt.Printf("%-20s %-15s %-5d\n", totp.Name, n, t)
-		}
-	}
-
-	return nil
-}
-
 var (
 	newSecret *Secret
 )
@@ -118,6 +43,108 @@ func init() {
 	}
 }
 
+// Add 增加
+func (s *Secret) Add(t *hltool.TOTP) error {
+	o, err := hltool.StructToBytes(t)
+	if err != nil {
+		return err
+	}
+	err = s.TwoStepDB.Set(map[string][]byte{
+		t.Name: o,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete 数据库中删除指定的name
+func (s *Secret) Delete(name string) error {
+	err := s.TwoStepDB.Delete([]string{name})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func SortMapByKey(m map[string][]byte) []string {
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	return keys
+}
+
+// formatPrint 格式化输出
+func formatPrint(r map[string][]byte)  {
+	for _, v := range SortMapByKey(r) {
+		totp := new(hltool.TOTP)
+		err := hltool.BytesToStruct(r[v], totp)
+		if err != nil {
+			continue
+		}
+		n, t, err := hltool.TwoStepAuthGenNumber(totp)
+		if err != nil {
+			continue
+		}
+		fmt.Printf("%-20s %-15s %-5d\n", totp.Name, n, t)
+	}
+}
+
+// List 列出所有的名称和6位数字
+func (s *Secret) List(name string) error {
+	fmt.Printf("%-20s %-15s %-5s\n", "Name", "Number", "Remaining time")
+	fmt.Printf("%-20s %-15s %-5s\n", "----", "------", "--------------")
+
+	if name != "all" {
+		r, err := s.TwoStepDB.Get([]string{name})
+		if err != nil {
+			return err
+		}
+		formatPrint(r)
+
+	} else {
+		r, err := s.TwoStepDB.GetAll()
+		if err != nil {
+			return err
+		}
+		formatPrint(r)
+	}
+
+	return nil
+}
+
+// Save 保存6位数字到文件
+func (s *Secret) Save(name, username, path string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	r, err := s.TwoStepDB.Get([]string{name})
+	if err != nil {
+		return err
+	}
+	for _, v := range r {
+		totp := new(hltool.TOTP)
+		err := hltool.BytesToStruct(v, totp)
+		if err != nil {
+			continue
+		}
+		n, _, err := hltool.TwoStepAuthGenNumber(totp)
+		if err != nil {
+			continue
+		}
+		fmt.Fprintf(f, "%s\n%s", username, n)
+	}
+
+	return nil
+}
+
 func cli() {
 	app := kingpin.New("google-authenticator-cli", "模拟 Google Authenticator 验证器")
 
@@ -128,10 +155,15 @@ func cli() {
 	algorithm := add.Flag("alg", "指定加密算法 SHA1|SHA256").Default("SHA1").String()
 
 	del := app.Command("delete", "删除secret")
-	deleteName := del.Flag("delete-secret", "名称标识").Required().String()
+	deleteName := del.Flag("delete-name", "名称标识").Required().String()
 
 	show := app.Command("show", "显示所有的6位数字")
-	showSecret := show.Flag("show-secret", "显示指定的标识的6位数字").Default("all").String()
+	showSecret := show.Flag("show-name", "显示指定的标识的6位数字").Default("all").String()
+
+	save := app.Command("save", "保存生成的6位数字到文件,文件格式: 第一行: 用户名  第二行: 6位数字")
+	saveName := save.Flag("save-name", "指定要保存的名称").Required().String()
+	username := save.Flag("username", "用户名,比如连接OPENVPN的用户名").Required().String()
+	savePath := save.Flag("path", "文件存储路径").Required().String()
 
 	c, err := app.Parse(os.Args[1:])
 	if err != nil {
@@ -168,6 +200,11 @@ func cli() {
 		err := newSecret.List(*showSecret)
 		if err != nil {
 			log.Fatalf("s.List(*showName) error: %s\n", err)
+		}
+	case "save":
+		err := newSecret.Save(*saveName, *username, *savePath)
+		if err != nil {
+			log.Fatalf("newSecret.Save(*saveName, *username, *savePath) error: %s\n", err)
 		}
 	}
 
